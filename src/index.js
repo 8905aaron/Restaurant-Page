64768395a49580eb432cae661000d31ab70e4cc6
Aaron <aaronfmccloskey@hotmail.co.uk>
2024-06-29 16:27:24 +0100

    First Release

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import restaurantImage from './assets/restaurant.jpg';
+import './styles.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+
+    const heading = document.createElement('h1');
+    heading.textContent = "Welcome to Aaron's Pizza Restaurant";
+
+    const image = document.createElement('img');
+    image.src = restaurantImage;
+    image.alt = 'Gourmet Paradise Restaurant';
+    
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Here we offer an exquisite dining experience'
+
+    const imgElement = document.createElement('img');
+    imgElement.src = restaurantImage;
+    
+    content.appendChild(heading);
+    content.appendChild(image);
+    content.appendChild(paragraph);
+});
+
+console.log('Hello World');
